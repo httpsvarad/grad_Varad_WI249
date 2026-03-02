6bf9c0459b728d5f25ce976e7db5c1bfbadf2e30
Varad <varad.manegopale28@gmail.com>
2026-03-03 01:18:32 +0530

    Jenkins GitHub Auto-Build

---
+++ jenkins-github-webhook-demo/CalculatorMain.java
@@ -0,0 +1,9 @@
+public class CalculatorMain{
+	public static void main(String[] args){
+	Calculator c1 = new Calculator();
+	System.out.println("ADD: "+c1.add(2,58));
+	System.out.println("SUBTRACT: "+c1.subtract(50,58));
+	System.out.println("MULTIPLY: "+c1.multiply(2,6));
+	System.out.println("DIVIDE: "+c1.divide(10,2));	
+	}
+}
